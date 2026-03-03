d3a17e56460052fa3b170fca6e4c0948bcdc23ee
aa4316 <aa3416@srmist.edu.in>
2026-03-03 15:33:09 +0530

    add Uc3

---
+++ OOPSBannerApp_Uc3.java
@@ -0,0 +1,16 @@
+public class OOPSBannerApp_Uc3 {
+
+    public static void main(String[] args) {
+        // Each println represents one row of the banner
+        System.out.println("    ***       ***    ******       *****  ");
+        System.out.println("  **   **   **   **  **    **   **       ");
+        System.out.println(" **     ** **     ** **     ** **        ");
+	    System.out.println(" **     ** **     ** **    **   **       ");
+	    System.out.println(" **     ** **     ** ******       ***    ");
+        System.out.println(" **     ** **     ** **              **  ");
+        System.out.println(" **     ** **     ** **               ** ");
+        System.out.println("  **   **   **   **  **              **  ");
+        System.out.println("    ***       ***    **         *****    ");
+
+    }
+}
